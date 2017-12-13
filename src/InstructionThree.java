d61e7fca2240b19e5d5433de749e2a7a5ae72c94
omarkhaledabdo <omarkhaledabdo@gmail.com>
2017-12-13 17:22:33 +0200

    Added Expression support

--- src/InstructionThree.java
+++ src/InstructionThree.java
@@ -50,6 +50,8 @@ public class InstructionThree extends Instruction {
 
         //System.out.println(mnemonic + " " + operand);
 
+        operand = operand.replace(" ", "");
+
         if (operand.startsWith("@")) { //indirect
             //invariants
             n = '1';
@@ -69,16 +71,24 @@ public class InstructionThree extends Instruction {
             n = '0';
             i = '1';
             x = '0';
-            // #BUFF, BUFF
-            if (asm.getSymTab().containsLabel(operand.substring(1))) {
-                String symb = operand.substring(1);
-                Integer targetAddr = asm.getSymTab().getAddress(symb);
+            String[] operands = operand.replace("#", "").split("(?=[-+])");
+            // etc: LDA #LENGTH
+            if(operands.length == 1 && asm.getSymTab().containsLabel(operands[0])) {
+                Integer targetAddr = asm.getSymTab().getAddress(operands[0]);
                 setRelativity(targetAddr, asm.getBaseAddr());
-            } else {
+            } else { //etc: LDA #LENGTH + LIST + 5, result must be absolute
                 b = '0';
                 p = '0';
-                Integer targetAddr = Integer.valueOf(operand.substring(1), 10);
-                disp = NumberUtils.adjustSize(Integer.toBinaryString(targetAddr), 12);
+                Integer absValue = 0;
+                operands[0] = "+" + operands[0];
+                for(String operand : operands) {
+                    if(asm.getSymTab().containsLabel(operand.substring(1))) {
+                        absValue = (operand.charAt(0) == '+' ? 1 : -1) *  asm.getSymTab().getAddress(operand.substring(1));
+                    } else {
+                        absValue += (operand.charAt(0) == '+' ? 1 : -1) * Integer.valueOf(operand.substring(1), 10);
+                    }
+                }
+                disp = NumberUtils.adjustSize(Integer.toBinaryString(absValue), 12);
             }
         } else if (operand.startsWith("=")) {
             n = '1';
@@ -88,16 +98,25 @@ public class InstructionThree extends Instruction {
             Integer targetAddr = asm.getLitTab().getLiteralAddr(val);
             setRelativity(targetAddr, asm.getBaseAddr());
         } else {  //Simple, op m, x OR op c, x
-            String[] tokens = operand.split("\\s*,\\s*");
+            String[] operands = operand.split("\\s*(?=[-+,])\\s*");
             n = '1';
             i = '1';
-            x = tokens.length == 1 ? '0' : '1';
-            String operandOne = tokens[0];
-            Integer targetAddr = asm.getSymTab().getAddress(operandOne);
+            //mem, X OR mem OR mem + constant
+            x = operands.length == 1  || !operands[1].equals(",") ? '0' : '1';
+
+            Integer targetAddr = asm.getSymTab().getAddress(operands[0]);
+//            System.out.println("Operands " + operands[0]);
+//            if(operands.length > 1)
+//            System.out.println("Operands " + operands[1]);
+
+            // +Constant
+            if (operands.length > 1 && operands[1].charAt(0) != ',' ) {
+                targetAddr += (operands[1].charAt(0) == '+' ? 1 : -1) * Integer.valueOf(operands[1].substring(1), 10);
+            }
+
             setRelativity(targetAddr, asm.getBaseAddr());
         }
         machineCode = trimmedOpcode() + n + i + x + b + p + e + disp;
         machineCode = NumberUtils.adjustSize(NumberUtils.binaryToHex(machineCode),6);
-        //System.out.println(machineCode);
     }
 }
